35ad0d0498f0f371150496249157a495a3310d53
Himanshi Rathore <108826224+himanshi-rathore@users.noreply.github.com>
2023-12-21 10:26:07 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -60,7 +60,7 @@ const Page = () => {
       <div className="w-full md:py-20">
         <Wrapper>
           <div className="text-center max-w-[800px] mx-auto mt-5 md:mt-0">
-            <div className="text-[22px] md:text-[26px] mb-5 font-semibold leading-tight">
+            <div className="text-[28px] md:text-[30px]  font-bold leading-tight text-center max-w-[800px] mx-auto mt-8 md:mt-0 mb-4">
               Shopping Cart
             </div>
           </div>
